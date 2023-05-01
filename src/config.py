125934bc527a56c9bf9a3535103040f4e31f8ff6
charlie890414 <charlie890414@gmail.com>
2023-05-02 01:06:57 +0800

    add language config

--- src/config.py
+++ src/config.py
@@ -4,10 +4,12 @@ from typing import Optional
 _default_configs = {
     "USER_AGENT": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
                   "AppleWebKit/537.36 (KHTML, like Gecko) "
-                  "Chrome/74.0.3729.169 Safari/537.36"
+                  "Chrome/74.0.3729.169 Safari/537.36",
+    "LANG": "en-us"
 }
 
 CONFIG_USER_AGENT = "USER_AGENT"
+CONFIG_LANG = "LANG"
 
 
 def get_config(key: str) -> Optional[str]:
